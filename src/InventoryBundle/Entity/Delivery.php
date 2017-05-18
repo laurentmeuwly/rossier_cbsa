851fa89494225effeb7519dbcb840c6a14079867
Laurent Meuwly <laurent.meuwly@camptocamp.com>
2017-05-18 16:19:11 +0200

    Display time with the date of delivery

--- src/InventoryBundle/Entity/Delivery.php
+++ src/InventoryBundle/Entity/Delivery.php
@@ -71,7 +71,7 @@ class Delivery
     /** {@inheritdoc} */
     public function __toString()
     {
-    	return 'Livraison du '. $this->getDeliveryDate()->format('d.m.Y');
+    	return 'Livraison du '. $this->getDeliveryDate()->format('d.m.Y H:i');
     }
     
     
